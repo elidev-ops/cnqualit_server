cb0b4899da375a853431207dc5cc2c45cff8dd0d
Elivelton S <55960381+elidev-ops@users.noreply.github.com>
2020-09-14 11:12:06 -0300

    Update serializeUrl.ts

--- src/util/serializeUrl.ts
+++ src/util/serializeUrl.ts
@@ -4,7 +4,7 @@ export default function serializeUrl(data: Product[]) {
   const SerializedProducts = data.map(product => {
     return {
       ...product,
-      image: `http://192.168.1.106:3001/uploads/${product.image}`
+      image: `http://api.cnquality.com.br/uploads/${product.image}`
     }
   });
   return SerializedProducts;
